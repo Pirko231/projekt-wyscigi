fe7f3308b27df6594fb5e7d27171941d06995922
szymek <szymek.pirowski@gmail.com>
2025-02-08 15:59:27 +0100

    Zmiana pliku cmake oraz male zmiany w pliku main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <optional>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Wyscigi");
+    sf::CircleShape circle{50.f};
+    circle.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            if (event->is<sf::Event::Closed>())
+            if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
 
         window.clear();
+        window.draw(circle);
         window.display();
     }
 }
