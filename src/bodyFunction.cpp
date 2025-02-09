ed63e8b83db113369fdf2b7bd587ebf9ef70614b
szymek <szymek.pirowski@gmail.com>
2025-02-09 17:55:27 +0100

    Maly remont - zmienienie lokalizacji typu wyliczeniowego na 'bodyFuncunction.h'. Mozliwosc zmieniania obecnego ekranu wewnatrz klas dziedziczacych od 'BodyFunction'

--- src/bodyFunction.cpp
+++ src/bodyFunction.cpp
@@ -1,10 +1,11 @@
 #include "bodyFunction.h"
 
-BodyFunction::BodyFunction(sf::RenderWindow *_window)
+BodyFunction::BodyFunction(sf::RenderWindow *_window, ManagingFunctionsIterator& _managingFunctionsIterator) : functionIterator{_managingFunctionsIterator}
 {
     this->window = _window;
 }
 
 BodyFunction::~BodyFunction()
 {
+
 }
