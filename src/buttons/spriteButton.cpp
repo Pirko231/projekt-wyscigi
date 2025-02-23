6a5cfceb4f659b9bcce45f1d4f08310e5d548788
szymek <szymek.pirowski@gmail.com>
2025-02-23 22:17:22 +0100

    Praca nad klasa textButton. zmiany w .gitignore

--- src/buttons/spriteButton.cpp
+++ src/buttons/spriteButton.cpp
@@ -1,19 +1,66 @@
 #include "spriteButton.h"
 
+void btn::SpriteButton::startClickAnimation(sf::Vector2f _maxAdditionalScale)
+{
+    this->locked = true;
+    this->animation--;
+    this->previousScale = this->sprite.getScale();
+    this->sprite.setScale({(this->maxScale.x + _maxAdditionalScale.x), (this->maxScale.y + _maxAdditionalScale.y)});
+}
+
 void btn::SpriteButton::clicked()
 {
+    if (this->animation == this->maxAnimation / 2)    
+        this->sprite.setScale(this->previousScale);
+    
+    this->animation--;
+}
+
+bool btn::SpriteButton::manage(bool _lockedInput)
+{
+
+    if (_lockedInput)
+        return false;
+
+    if (this->isAnimated()) 
+        this->clicked();
+
+    if (this->isAnimationFinished())
+        return true;
+    
+    return false;
 }
 
 void btn::SpriteButton::howered()
 {
-    /*if (this->sprite.getScale() < {this->maxScale.x, this->maxScale.y})
+    if (this->locked)
+        return;
+    
+    if (this->sprite.getScale().x < this->maxScale.x && this->sprite.getScale().y < this->maxScale.y)
     {
-        this->sprite.setScale({this->sprite.getScale().x + this->changeBy, this->sprite.getScale().y + this->changeBy});
-        this->hitBox.setSize({this->text.getLocalBounds().width, static_cast<float>(this->text.getLocalBounds().height * 1.35)});
+        this->sprite.setScale(this->sprite.getScale() + this->changeBy);
+        //this->hitBox.setSize({this->sprite.getLocalBounds().width, static_cast<float>(this->sprite.getLocalBounds().height * 1.35)});
     }
     else
     {
-        this->text.setCharacterSize(this->maxCharacterSize);
-        this->hitBox.setSize({this->text.getLocalBounds().width, static_cast<float>(this->text.getLocalBounds().height * 1.35)});
-    }*/
+        this->sprite.setScale(this->maxScale);
+        //this->hitBox.setSize({this->sprite.getLocalBounds().width, static_cast<float>(this->sprite.getLocalBounds().height * 1.35)});
+    }
+}
+
+void btn::SpriteButton::unHowered()
+{
+    if (this->locked)
+        return;
+
+    if (this->sprite.getScale().x > this->defaultScale.x && this->sprite.getScale().y > this->defaultScale.y)
+    {
+        this->sprite.setScale(this->sprite.getScale() - this->changeBy);
+        //this->hitBox.setSize({this->sprite.getLocalBounds().width, this->sprite.getLocalBounds().height * 1.35f});
+    }
+    else
+    {
+        this->sprite.setScale(this->defaultScale);
+        //this->hitBox.setSize({this->sprite.getLocalBounds().width, this->sprite.getLocalBounds().height * 1.35f});
+    }
 }
--- src/buttons/spriteButton.h
+++ src/buttons/spriteButton.h
@@ -23,22 +23,39 @@ namespace btn
         //ustawia pozycje na _pos
         void setPosition(sf::Vector2f _pos) {this->sprite.setPosition(_pos);}
 
+        void reset() {this->animation = this->maxAnimation; this->locked = false; this->setScale(this->defaultScale);}
+
         //skaluje obiekt na _scale
         //_maxScale - maksymalna skala podczas animacji. Kiedy nie ruszona zostanie sama obliczona
-        void setScale(sf::Vector2f _scale, sf::Vector2f _maxScale = {0.f,0.f}) {this->sprite.setScale(_scale); this->defaultScale = _scale; if(_maxScale == sf::Vector2f{0.f,0.f}) this->maxScale = {_scale.x * 1.3f, _scale.y * 1.3f}; else this->maxScale = _maxScale;}
+        //_speed - przyspiesza animacje
+        void setScale(sf::Vector2f _scale, sf::Vector2f _maxScale = {0.f,0.f}, int _speed = 9) 
+        {
+            this->sprite.setScale(_scale); 
+            this->defaultScale = _scale; 
+            if(_maxScale == sf::Vector2f{0.f,0.f}) _maxScale = {_scale.x * 1.35f, _scale.y * 1.35f};
+            this->maxScale = _maxScale;
+            this->changeBy = {(_maxScale.x - _scale.x) / this->framerate * _speed, (_maxScale.y - _scale.y) / this->framerate * _speed};
+        }
 
         //ustawia kąt rotacji na _angle
         void setRotation(float _angle) {this->sprite.setRotation(_angle);}
         
         //zwraca hitbox obrazu aby mozna bylo porownac to z pozycja myszki
-        sf::FloatRect getHitbox() {return this->hitbox.getGlobalBounds();}
+        sf::FloatRect getHitbox() {return this->sprite.getGlobalBounds();/*return this->hitBox.getGlobalBounds();*/}
         
         //uzyc kiedy przycisk zostal klikniety
-        void clicked();
+        //_maxAdditionalScale - okresla o ile wiekszy bedzie przycisk od maksymalnej skali po kliknieciu
+        void startClickAnimation(sf::Vector2f _maxAdditionalScale = {0.07f, 0.07f});
+
+        //wystarczy wywolac i samo sprawdzi czy ma robic animacje klikniecia
+        //_lockedInput - mozna zablokowac dzialanie funkcji
+        bool manage(bool _lockedInput = false);
         
         //jezeli jest najechany myszka (uzyj .getHitbox()) aby sprawdzic
         void howered();
 
+        //jezeli nie jest najechany myszka (uzyj .getHitbox()) aby sprawdzic
+        void unHowered();
         
     private:
         void draw(sf::RenderTarget &target, sf::RenderStates states) const override
@@ -46,8 +63,29 @@ namespace btn
             target.draw(this->sprite, states);
         }
 
+        //sprawdzic czy trwa animacja i kiedy trwa to uzyc
+        void clicked();
+
+        //zwraca true jesli trwa animacja. w przeciwnym razie zwraca false
+        bool isAnimated() const {return this->animation < this->maxAnimation;}
+
+        //zwraca true kiedy animacja jest skonczona. wprzeciwnym razie zwraca flse
+        bool isAnimationFinished() const {return this->animation <= 0;}
+
         sf::Sprite sprite;
-        sf::RectangleShape hitbox;
+        sf::RectangleShape hitBox;
+
+        //jezeli klikniety to blokuje powiekszanie i pomniejszanie
+        bool locked{false};
+        
+        //klatki na sekunde
+        int framerate{60};
+
+        //ilosc maksymalnych klatek do zakonczenia animacji
+        int maxAnimation{23};
+
+        //ilosc klatek do zakonczenia animacji
+        int animation{this->maxAnimation};
 
         //podstawowa skala obiektu
         sf::Vector2f defaultScale{1.f,1.f};
@@ -56,8 +94,10 @@ namespace btn
         sf::Vector2f maxScale{1.2f,1.2f};
 
         //o ile skala ma sie zmianiac na sekunde
-        float changeBy;
+        sf::Vector2f changeBy;
 
+        //zapisuje skale po nacisnieciu przycisku
+        sf::Vector2f previousScale;
     };
 
 };
