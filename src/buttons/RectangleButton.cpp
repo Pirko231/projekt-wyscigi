1e77b882cbe44e9094ed801cef7e82a8e0427d56
kaluzka17 <kaluzka17@gmail.com>
2025-02-26 16:42:51 +0100

    Dodano klasę RectangleButton

---
+++ src/buttons/RectangleButton.cpp
@@ -0,0 +1,69 @@
+#include "RectangleButton.h"
+
+btn::RectangleButton::RectangleButton(sf::Vector2f _pos, sf::Vector2f _size, sf::Color _color, sf::Color _hoverColor)
+{
+    rectangle.setPosition(_pos);
+    rectangle.setSize(_size);
+    rectangle.setFillColor(_color);
+
+    this->baseColor = _color;
+    this->hoverColor = _hoverColor;
+}
+
+void btn::RectangleButton::setPosition(sf::Vector2f _pos)
+{
+    rectangle.setPosition(_pos);
+}
+
+void btn::RectangleButton::setSize(sf::Vector2f _size)
+{
+    rectangle.setSize(_size);
+}
+
+void btn::RectangleButton::setColor(sf::Color _color)
+{
+    rectangle.setFillColor(_color);
+    this->baseColor = _color;
+}
+
+void btn::RectangleButton::setHoverColor(sf::Color _hoverColor)
+{
+    this->hoverColor = _hoverColor;
+}
+
+void btn::RectangleButton::setTexture(const sf::Texture& _texture)
+{
+    sprite.setTexture(_texture);
+    sprite.setPosition(rectangle.getPosition());
+}
+
+void btn::RectangleButton::manageHover(sf::Vector2i mousePos)
+{
+    if (rectangle.getGlobalBounds().contains(sf::Vector2f(mousePos)))
+    {
+        if (!isHovered)
+        {
+            rectangle.setFillColor(hoverColor);  // Zmieniamy kolor na hover
+            isHovered = true;
+        }
+    }
+    else
+    {
+        if (isHovered)
+        {
+            rectangle.setFillColor(baseColor);  // Przywracamy kolor początkowy
+            isHovered = false;
+        }
+    }
+}
+
+void btn::RectangleButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(rectangle, states);
+
+    // Jeśli jest tekstura, rysujemy ją w środku przycisku
+    if (sprite.getTexture())
+    {
+        target.draw(sprite, states);
+    }
+}
