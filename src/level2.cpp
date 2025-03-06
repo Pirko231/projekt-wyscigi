d6a56651ab91c1dd53648abadb3ab0330d0dd588
szymek <szymek.pirowski@gmail.com>
2025-03-06 19:02:39 +0100

    wczytanie poziomu 2

--- src/level2.cpp
+++ src/level2.cpp
@@ -2,6 +2,17 @@
 
 Level2::Level2(sf::RenderWindow *_window, sf::Mouse *_mouse, ManagingFunctionsIterator &_managingFunctionsIterator, Settings *_settings, sf::Music *_music) : Level{_window, _mouse, _managingFunctionsIterator, _settings, _music}
 {
+    sf::Texture mapTxt;
+
+    Report report;
+    report.open();
+    report.logMessage("Level2");
+
+    report.addEntry("Mapa", mapTxt.loadFromFile("resources/SPEEDWAY2.0..jpg"));
+
+    report.close();
+    
+    this->loadLevel(mapTxt);
 }
 
 /*void Level2::handleEvents(sf::Event &_event)
