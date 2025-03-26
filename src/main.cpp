1a14529b5b4169acb4225bc3b7cea7c2b35d91a4
Pirko231 <szymek.pirowski@gmail.com>
2025-03-26 14:14:06 +0100

    pomiar predkosci

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "program.h"
 
+#ifdef SPEEDTEST
+#include "speedTest.h"
+#endif
+
 int main()
 {
     Program program;
@@ -8,8 +12,34 @@ int main()
     {
         //tutaj nie dodajemy juz niczego
         //wszystko ma sie odbywac w tych trzech funkcjach
+#if SPEEDTEST
+        if (perf::SpeedTracker::get().working())
+            perf::SpeedTracker::get().frameClock.restart();
+        sf::Time currentTime;
+#endif
         program.handleEvents();
+#if SPEEDTEST
+        if (perf::SpeedTracker::get().working())
+            perf::SpeedTracker::get().handleEventsTime = perf::SpeedTracker::get().frameClock.getElapsedTime() - currentTime;
+        currentTime = perf::SpeedTracker::get().frameClock.getElapsedTime();
+#endif
         program.update();
+#if SPEEDTEST
+        if (perf::SpeedTracker::get().working())
+            perf::SpeedTracker::get().updateTime = perf::SpeedTracker::get().frameClock.getElapsedTime() - currentTime;
+        currentTime = perf::SpeedTracker::get().frameClock.getElapsedTime();
+#endif
         program.display();
+#if SPEEDTEST
+        if (perf::SpeedTracker::get().working())
+            perf::SpeedTracker::get().displayTime = perf::SpeedTracker::get().frameClock.getElapsedTime() - currentTime;
+        currentTime = perf::SpeedTracker::get().frameClock.getElapsedTime();
+#endif
+
+#if SPEEDTEST
+        if (perf::SpeedTracker::get().working())
+            perf::SpeedTracker::get().frameTime = perf::SpeedTracker::get().frameClock.getElapsedTime();
+        perf::SpeedTracker::get().update();
+#endif
     }
 }
