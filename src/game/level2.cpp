92d5dac300904522891da553551069ada3e77990
kaluzka17 <kaluzka17@gmail.com>
2025-03-29 14:29:43 +0100

    checkpointy level2

--- src/game/level2.cpp
+++ src/game/level2.cpp
@@ -13,9 +13,29 @@ Level2::Level2(sf::RenderWindow *_window, sf::Mouse *_mouse, ManagingFunctionsIt
     report.close();
     
     this->loadLevel(mapTxt, {-320, -180});
-
-    this->checkPoints.push_back(bdr::CheckPoint{{400.f, 400.f}, {20.f, 100.f}});
-    this->checkPoints.push_back(bdr::CheckPoint{{500.f, 400.f}, {20.f, 100.f}});
+    //ustawienie checkpoint
+    this->checkPoints.push_back(bdr::CheckPoint{{470.f, 609.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{288.f, 528.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{170.f, 458.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{40.f, 370.f}, {120.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{97.f, 257.f}, {120.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{164.f, 127.f}, {120.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{324.f, 15.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{470.f, 53.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{622.f, 60.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{995.f, 58.f}, {20.f, 110.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1048.f, 185.f}, {110.f, 10.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1048.f, 235.f}, {110.f, 10.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1018.f, 269.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{774.f, 280.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{518.f, 284.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{398.f, 405.f}, {100.f, 10.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{510.f, 445.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1024.f, 453.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1063.f, 568.f}, {100.f, 10.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1024.f, 610.f}, {20.f, 100.f}});
+    //meta
+    this->checkPoints.push_back(bdr::CheckPoint{{585.f, 610.f}, {20.f, 100.f}});
 }
 
 void Level2::resetCurrentLevel()
