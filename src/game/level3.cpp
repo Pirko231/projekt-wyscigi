02d2d72d47c12db3a7f0c89b9a1ecc14111c67a2
kaluzka17 <kaluzka17@gmail.com>
2025-03-29 15:11:39 +0100

    checkpointy level3

--- src/game/level3.cpp
+++ src/game/level3.cpp
@@ -14,7 +14,29 @@ Level3::Level3(sf::RenderWindow *_window, sf::Mouse *_mouse, ManagingFunctionsIt
     
     this->loadLevel(mapTxt, {-320, -180});
 
-    this->checkPoints.push_back(bdr::CheckPoint{{400.f, 400.f}, {20.f, 100.f}});
+    //ustawienie checkpointow
+    this->checkPoints.push_back(bdr::CheckPoint{{189.f, 602.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{36.f, 549.f}, {110.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{35.f, 174.f}, {110.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{159.f, 25.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{240.f, 25.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{266.f, 173.f}, {100.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{265.f, 210.f}, {100.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{394.f, 244.f}, {20.f, 120.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{849.f, 259.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{968.f, 259.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1036.f, 259.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1082.f, 200.f}, {100.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{1020.f, 43.f}, {20.f, 100.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{862.f, 184.f}, {100.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{853.f, 370.f}, {120.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{919.f, 526.f}, {120.f, 20.f}});
+    this->checkPoints.push_back(bdr::CheckPoint{{863.f, 602.f}, {20.f, 110.f}});
+
+
+
+    //meta
+    this->checkPoints.push_back(bdr::CheckPoint{{493.f, 603.f}, {20.f, 100.f}});
 }
 
 void Level3::resetCurrentLevel()
